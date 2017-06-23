e1ce7b49a36645338e265babe4ef6f75d2e64a8a
Michael Cordingley <Michael.Cordingley@gmail.com>
2017-06-23 17:50:34 -0400

    Define package configuration.

---
+++ src/config.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'shared'  => [
+        'authentication_key' => env('SAPIENT_SHARED_AUTHENTICATION_KEY'),
+        'encryption_key'     => env('SAPIENT_SHARED_ENCRYPTION_KEY'),
+    ],
+    'sealing' => [
+        'private_key' => env('SAPIENT_SEALING_PRIVATE_KEY'),
+        'public_key'  => env('SAPIENT_SEALING_PUBLIC_KEY'),
+    ],
+    'signing' => [
+        'private_key' => env('SAPIENT_SIGNING_PRIVATE_KEY'),
+        'public_key'  => env('SAPIENT_SIGNING_PUBLIC_KEY'),
+    ],
+];
