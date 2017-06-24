65c5eb9f40d2678393b0918ae23529c5295bd284
Michael Cordingley <Michael.Cordingley@gmail.com>
2017-06-24 00:01:32 -0400

    Add symmetric encrypt and decrypt middleware.

---
+++ src/Middleware/SharedDecryptRequest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace MCordingley\LaravelSapient\Middleware;
+
+use Closure;
+use function GuzzleHttp\Psr7\stream_for;
+use Illuminate\Http\Request;
+use ParagonIE\Sapient\CryptographyKeys\SharedEncryptionKey;
+use ParagonIE\Sapient\Simple;
+use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Component\HttpFoundation\Response;
+use Zend\Diactoros\ServerRequest as DiactorosRequest;
+
+final class SharedDecryptRequest
+{
+    /** @var DiactorosFactory */
+    private $psrFactory;
+
+    /** @var HttpFoundationFactory */
+    private $symfonyFactory;
+
+    /** @var SharedEncryptionKey */
+    private $key;
+
+    /**
+     * @param SharedEncryptionKey $key
+     */
+    public function __construct(SharedEncryptionKey $key)
+    {
+        $this->psrFactory = new DiactorosFactory;
+        $this->symfonyFactory = new HttpFoundationFactory;
+        $this->key = $key;
+    }
+
+
+    /**
+     * @param Request $request
+     * @param Closure $next
+     * @return Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        /** @var DiactorosRequest $psrRequest */
+        $psrRequest = $this->psrFactory->createRequest($request);
+
+        $plainText = Simple::decrypt($psrRequest->getBody(), $this->key);
+        $psrRequest = $psrRequest->withBody(stream_for($plainText));
+        $request = Request::createFromBase($this->symfonyFactory->createRequest($psrRequest));
+
+        app()->instance('request', $request);
+
+        return $next($request);
+    }
+}
---
+++ src/Middleware/SharedEncryptResponse.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace MCordingley\LaravelSapient\Middleware;
+
+use Closure;
+use function GuzzleHttp\Psr7\stream_for;
+use Illuminate\Http\Request;
+use ParagonIE\Sapient\CryptographyKeys\SharedEncryptionKey;
+use ParagonIE\Sapient\Simple;
+use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Component\HttpFoundation\Response;
+use Zend\Diactoros\Response as DiactorosResponse;
+
+final class SharedEncryptResponse
+{
+    /** @var DiactorosFactory */
+    private $psrFactory;
+
+    /** @var HttpFoundationFactory */
+    private $symfonyFactory;
+
+    /** @var SharedEncryptionKey */
+    private $key;
+
+    /**
+     * @param SharedEncryptionKey $key
+     */
+    public function __construct(SharedEncryptionKey $key)
+    {
+        $this->psrFactory = new DiactorosFactory;
+        $this->symfonyFactory = new HttpFoundationFactory;
+        $this->key = $key;
+    }
+
+    /**
+     * @param Request $request
+     * @param Closure $next
+     * @return Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        /** @var Response $response */
+        $response = $next($request);
+
+        /** @var DiactorosResponse $psrResponse */
+        $psrResponse = $this->psrFactory->createResponse($response);
+        $cipherText = Simple::decrypt($psrResponse->getBody(), $this->key);
+
+        return $this->symfonyFactory->createResponse($psrResponse->withBody(stream_for($cipherText)));
+    }
+}
