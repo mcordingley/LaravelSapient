c24c94e5cd443c366a1f0ba98806031a18ef9c4a
Michael Cordingley <Michael.Cordingley@gmail.com>
2017-06-25 14:44:00 -0400

    DRY up request replacement.

--- src/Middleware/UnsealRequest.php
+++ src/Middleware/UnsealRequest.php
@@ -3,23 +3,15 @@
 namespace MCordingley\LaravelSapient\Middleware;
 
 use Closure;
-use function GuzzleHttp\Psr7\stream_for;
 use Illuminate\Http\Request;
+use MCordingley\LaravelSapient\ReplacesRequests;
 use ParagonIE\Sapient\CryptographyKeys\SealingSecretKey;
 use ParagonIE\Sapient\Simple;
-use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
-use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
-use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Component\HttpFoundation\Response;
-use Zend\Diactoros\ServerRequest as DiactorosRequest;
 
 final class UnsealRequest
 {
-    /** @var DiactorosFactory */
-    private $psrFactory;
-
-    /** @var HttpFoundationFactory */
-    private $symfonyFactory;
+    use ReplacesRequests;
 
     /** @var SealingSecretKey */
     private $key;
@@ -29,8 +21,6 @@ final class UnsealRequest
      */
     public function __construct(SealingSecretKey $key)
     {
-        $this->psrFactory = new DiactorosFactory;
-        $this->symfonyFactory = new HttpFoundationFactory;
         $this->key = $key;
     }
 
@@ -43,25 +33,7 @@ final class UnsealRequest
     public function handle(Request $request, Closure $next): Response
     {
         if (!in_array($request->method(), ['HEAD', 'GET', 'OPTIONS'])) {
-            $data = Simple::unseal($request->getContent(), $this->key);
-
-            /** @var DiactorosRequest $psrRequest */
-            $psrRequest = $this->psrFactory->createRequest($request);
-            $psrRequest = $psrRequest->withBody(stream_for($data));
-
-            $symfonyRequest = $this->symfonyFactory->createRequest($psrRequest);
-
-            if (0 === strpos($symfonyRequest->headers->get('CONTENT_TYPE'), 'application/x-www-form-urlencoded')
-                && in_array(strtoupper($symfonyRequest->server->get('REQUEST_METHOD', 'GET')), ['DELETE', 'PATCH', 'POST', 'PUT'])
-            ) {
-                $parsed = [];
-                parse_str($data, $parsed);
-                $symfonyRequest->request = new ParameterBag($parsed);
-            }
-
-            $request = Request::createFromBase($symfonyRequest);
-
-            app()->instance('request', $request);
+            $request = $this->replaceRequest($request, Simple::unseal($request->getContent(), $this->key));
         }
 
         return $next($request);
---
+++ src/ReplacesRequests.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace MCordingley\LaravelSapient;
+
+use function GuzzleHttp\Psr7\stream_for;
+use Illuminate\Http\Request;
+use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
+use Zend\Diactoros\ServerRequest as DiactorosRequest;
+
+trait ReplacesRequests
+{
+    /**
+     * @param Request $request
+     * @param string $newBody
+     * @return Request
+     */
+    final protected function replaceRequest(Request $request, string $newBody): Request
+    {
+        $psrFactory = new DiactorosFactory;
+        $symfonyFactory = new HttpFoundationFactory;
+
+        /** @var DiactorosRequest $psrRequest */
+        $psrRequest = $psrFactory->createRequest($request);
+        $psrRequest = $psrRequest->withBody(stream_for($newBody));
+
+        /** @var SymfonyRequest $symfonyRequest */
+        $symfonyRequest = $symfonyFactory->createRequest($psrRequest);
+        $symfonyRequest->headers->set('CONTENT_LENGTH', strlen($newBody));
+
+        if (0 === strpos($symfonyRequest->headers->get('CONTENT_TYPE'), 'application/x-www-form-urlencoded')
+            && in_array(strtoupper($symfonyRequest->server->get('REQUEST_METHOD', 'GET')), ['DELETE', 'PATCH', 'POST', 'PUT'])
+        ) {
+            $parsed = [];
+            parse_str($newBody, $parsed);
+            $symfonyRequest->request = new ParameterBag($parsed);
+        }
+
+        $request = Request::createFromBase($symfonyRequest);
+
+        app()->instance('request', $request);
+
+        return $request;
+    }
+}
