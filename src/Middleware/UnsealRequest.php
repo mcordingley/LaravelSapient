fcc3b7e2d0fa9fe1221083b7c4d21758a6dd939e
Michael Cordingley <Michael.Cordingley@gmail.com>
2017-06-25 14:29:32 -0400

    Reparse the body.

--- src/Middleware/UnsealRequest.php
+++ src/Middleware/UnsealRequest.php
@@ -9,6 +9,7 @@ use ParagonIE\Sapient\CryptographyKeys\SealingSecretKey;
 use ParagonIE\Sapient\Simple;
 use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
 use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Component\HttpFoundation\ParameterBag;
 use Symfony\Component\HttpFoundation\Response;
 use Zend\Diactoros\ServerRequest as DiactorosRequest;
 
@@ -42,12 +43,23 @@ final class UnsealRequest
     public function handle(Request $request, Closure $next): Response
     {
         if (!in_array($request->method(), ['HEAD', 'GET', 'OPTIONS'])) {
+            $data = Simple::unseal($request->getContent(), $this->key);
+
             /** @var DiactorosRequest $psrRequest */
             $psrRequest = $this->psrFactory->createRequest($request);
+            $psrRequest = $psrRequest->withBody(stream_for($data));
 
-            $plainText = Simple::unseal($psrRequest->getBody(), $this->key);
-            $psrRequest = $psrRequest->withBody(stream_for($plainText));
-            $request = Request::createFromBase($this->symfonyFactory->createRequest($psrRequest));
+            $symfonyRequest = $this->symfonyFactory->createRequest($psrRequest);
+
+            if (0 === strpos($symfonyRequest->headers->get('CONTENT_TYPE'), 'application/x-www-form-urlencoded')
+                && in_array(strtoupper($symfonyRequest->server->get('REQUEST_METHOD', 'GET')), ['DELETE', 'PATCH', 'POST', 'PUT'])
+            ) {
+                $parsed = [];
+                parse_str($data, $parsed);
+                $symfonyRequest->request = new ParameterBag($parsed);
+            }
+
+            $request = Request::createFromBase($symfonyRequest);
 
             app()->instance('request', $request);
         }
